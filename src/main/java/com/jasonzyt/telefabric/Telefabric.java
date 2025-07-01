5d35d12f6fdc9784011935c9f07492cae58af475
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-07-02 02:58:37 +0800

    feat: init project

---
+++ src/main/java/com/jasonzyt/telefabric/Telefabric.java
@@ -0,0 +1,10 @@
+package com.jasonzyt.telefabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Telefabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
